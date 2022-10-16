572d69baf3ee04c0697d3d267f3db298d8e9d68d
mmesoma <mmesomapreciousokeke@gmail.com>
2022-10-16 11:15:14 -0700

    task 1 - 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchdar(c + '0');
+			c++
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putcharr(c);
+			c++
+		}
+	putcharr('\n');
+	return (0);
+}
