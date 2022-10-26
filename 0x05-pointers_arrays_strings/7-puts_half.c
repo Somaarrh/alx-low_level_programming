91ac780a70eabf6a29dca24ca9a62835f0de4a68
mmesoma <mmesomapreciousokeke@gmail.com>
2022-10-25 17:04:36 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,14 @@
  */
 void puts_half(char *str)
 {
-	int len = 0
-
-		while (*str != '\0')
-		{
-			len++;
-			str++;
-		}
-
+	int len = 0;
+	
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	
 	str -= (len / 2);
 	while (*str != '\0')
 	{
